eba09783d7e606ef45e1168f623bf48168f2a9a6
IsaacShrestha@github.com <ishrestha@unomaha.edu>
2016-09-17 10:57:00 -0700

    End of task 17, photo feed working

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -19,7 +19,7 @@ import Ember from 'ember';
 });*/
 
 var PhotoCollection = Ember.ArrayProxy.extend(Ember.SortableMixin, {
-	sortProperties: ['title'],
+	sortProperties: ['dates.taken'],
 	sortAscending: true,
 	content: [],
 });
@@ -35,7 +35,7 @@ export default Ember.Controller.extend({
 		var photos = this.get('photos');
 
 		return photos.filter(function(photo){
-			return photo.get('title').match(rx) || photo.get('username').match(rx);
+			return photo.get('title').match(rx) || photo.get('owner.username').match(rx);
 		});
 	}.property('photos.@each', 'searchField'),
 
@@ -45,7 +45,6 @@ export default Ember.Controller.extend({
 		var host = 'https://api.flickr.com/services/rest/';
 		var method = "flickr.tags.getHotList";
 		var requestURL = host + "?method="+method + "&api_key="+apiKey+"&format=json&nojsoncallback=1";//+"&api_sig="+apiSig;
-		var photos = this.get('photos');
 		var t = this;
 		Ember.$.getJSON(requestURL, function(data){
 			//callback for successfully completed requests
@@ -67,27 +66,37 @@ export default Ember.Controller.extend({
 		getPhotos: function(tag){
 			var apiKey = 'aff9a42d0f8aefc47aa09da1597b59a0';
 			var host = 'https://api.flickr.com/services/rest/';
-			var method = "flickr.tags.getClusterPhotos";
+			var method = "flickr.photos.search";
 			//var apiSig = 'd44f5ca2979b0d53c80515319126ce5d';
-			var requestURL = host + "?method="+method + "&api_key="+apiKey+"&format=json&nojsoncallback=1";//+"&api_sig="+apiSig;
+			var requestURL = host + "?method="+method + "&api_key="+apiKey+"&tags="+tag+"&per_page=50&format=json&nojsoncallback=1";
 			var photos = this.get('photos');
 			var t = this;
 			Ember.$.getJSON(requestURL, function(data){
 				//callback for successfully completed requests
-				console.log(data);
-				data.photos.photo.map(function(photo){
-					var newPhotoItem = t.store.createRecord('photo',{
-						title: photo.title,
-						username: photo.username,
-						//flickr extra data
+				//make secondary requests to get all of the photo information
+				data.photos.photo.map(function(photoitem){
+					var infoRequestURL = host + "?method="+"flickr.photos.getInfo" + "&api_key="+apiKey+ "&photo_id="+photoitem.id+"&format=json&nojsoncallback=1";
+					Ember.$.getJSON(infoRequestURL, function(item){
+						var photo = item.photo;
+						var tags = photo.tags.tag.map(function(tagitem){
+							return tagitem._content;
+						});
+						var newPhotoItem = t.store.createRecord('photo',{
+						title: photo.title._content,
+						dates: photo.dates,
 						owner: photo.owner,
+						description: photo.description._content,
+						link: photo.urls.url[0]._content,
+						views: photo.views,
+						tags: tags,
 						//flickr url data
 						id: photo.id,
 						farm: photo.farm,
 						secret: photo.secret,
 						server: photo.server,
-					});
+						});
 					photos.pushObject(newPhotoItem);
+					});
 				});
 			});
 		},
